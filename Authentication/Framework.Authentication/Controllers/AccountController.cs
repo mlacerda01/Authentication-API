f84ff54ab39129434a4ecf89501acde1eff13d58
mlacerda01 <marttalacerda@gmail.com>
2020-06-22 22:22:41 -0300

    Criação de API de Authenticação

---
+++ Authentication/Framework.Authentication/Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Framework.Authentication.Models;
+using Framework.Authentication.Repositories;
+using Framework.Authentication.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Framework.Authentication.Controllers
+{
+    [Route("v1/account")]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost]
+        [Route("login")]
+        [AllowAnonymous]
+        public async Task<ActionResult<dynamic>> Authenticate([FromBody] User model)
+        {
+            var user = UserRepository.Get(model.UserName, model.Password);
+
+            if (user == null)
+                return NotFound(new { message = "Usuário ou senha inválidos!" });
+
+            var token = TokenService.GenerateToken(user);
+            user.Password = string.Empty;
+
+            return new
+            {
+                user = user,
+                token = token
+            };
+        }
+
+        [HttpGet]
+        [Route("anonymous")]
+        [AllowAnonymous]
+        public string Anonynous() => "Anônimo";
+
+        [HttpGet]
+        [Route("authenticated")]
+        [Authorize]
+        public string Authenticated() => string.Format("Autenticado - {0}", User.Identity.Name);
+
+        [HttpGet]
+        [Route("employee")]
+        [Authorize(Roles = "employee,manager")]
+        public string Employee() => string.Format("Funcionário");
+
+        [HttpGet]
+        [Route("manager")]
+        [Authorize(Roles = "manager")]
+        public string Manager() => string.Format("Gerente");
+    }
+}
